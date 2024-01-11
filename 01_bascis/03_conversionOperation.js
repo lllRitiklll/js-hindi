b7075a2f7e196d359ea6ddff9e804002edb16ba9
lllRitiklll <82300794+lllRitiklll@users.noreply.github.com>
2024-01-11 04:32:10 +0000

    conversion

---
+++ 01_bascis/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "23ac"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let ValueInNumber = Number(score)
+console.log(score)
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; , false => 0;
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Ritik" => true
+
+
+let someNumber = 33
+let convertNumberInToString = String(someNumber)
+console.log(convertNumberInToString)
+console.log(typeof someNumber)
+console.log(typeof convertNumberInToString)
