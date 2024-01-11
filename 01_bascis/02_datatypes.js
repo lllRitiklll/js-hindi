326b20f4a766734b0aa8261071bf36efdb004d95
lllRitiklll <82300794+lllRitiklll@users.noreply.github.com>
2024-01-11 04:04:04 +0000

    detatypes

---
+++ 01_bascis/02_datatypes.js
@@ -0,0 +1,25 @@
+"use Strict"; //treat all js code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Ritik")
+
+let name ="Ritik"
+let age = 23
+let isLoggedIn = false
+
+// number =>2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assingn
+// symbol => for uniqueness
+// object
+
+
+console.log( typeof null) // object
+console.log( typeof undefined)// undefined
+
