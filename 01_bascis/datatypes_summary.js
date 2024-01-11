422bb142f2b9bf67d1e44536e0ea07c6419f88b8
lllRitiklll <82300794+lllRitiklll@users.noreply.github.com>
2024-01-11 14:59:32 +0000

    datatype summerary

---
+++ 01_bascis/datatypes_summary.js
@@ -0,0 +1,34 @@
+// primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoveVal = 100.3;
+
+const isLoggedIn=false;
+const outsideTemp =null;
+let userEmail;
+
+const id =Symbol('123')
+const id2 =Symbol('123')
+console.log(id == id2);
+
+const bigNumber = 293200000203920392n
+
+
+
+// Reference (non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Shaktiman" , "naagraj", "doga"]
+console.log(heros);
+const myObj = {
+    name:"Ritik",
+    age:23,
+}
+console.log(myObj);
+
+const myFunction =function(){
+    console.log("daddy........");
+}
